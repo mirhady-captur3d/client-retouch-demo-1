268557921bafe349d06f63681193230991eb3853
Mirhady Dorodjatun <mirhady@captur3d.io>
2025-02-06 09:19:55 +1100

    make layout wider

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,21 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 import cv2
 
+st.markdown("""
+    <style>
+    .main .block-container {
+        max-width: 100% !important;
+        padding-left: 2rem;
+        padding-right: 2rem;
+    }
+    .center-container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 
 st.set_page_config("Captur3d AI photo retouching alpha - demo test", "🔭", layout="centered")
 
@@ -12,7 +27,10 @@ st.write("")
 "A combination of automated color correction tasks and photo upscaling"
 st.write("")
 
-def centered_image_comparison(img1, img2, label1, label2, width=1000):
+def centered_image_comparison(img1, img2, label1, label2, width=1200):
+
+    st.markdown(f'<div class="center-container">', unsafe_allow_html=True)
+
     # Use columns to create a centered layout
     # left, mid, right = st.columns([1, 10, 5])
     # with mid:
@@ -23,6 +41,8 @@ def centered_image_comparison(img1, img2, label1, label2, width=1000):
         label2=label2,
         width=width  # 1.5x original width (assuming original was ~600px)
     )
+
+    st.markdown('</div>', unsafe_allow_html=True)
 
 st.markdown("### test image 1")
 centered_image_comparison(
