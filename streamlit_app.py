fe82d28d1b2c12252d3650bcc7d25c61166e294b
Mirhady Dorodjatun <mirhady@captur3d.io>
2025-02-06 16:29:15 +1100

    add more photos

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,3 +127,35 @@ centered_image_comparison(
     label1="input",
     label2="magic retouch",
 )
+
+st.markdown("### test image 11")
+centered_image_comparison(
+    img1="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/11.jpg",
+    img2="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/11_after.jpg",
+    label1="input",
+    label2="magic retouch",
+)
+
+st.markdown("### test image 12")
+centered_image_comparison(
+    img1="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/12.jpg",
+    img2="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/12_after.jpg",
+    label1="input",
+    label2="magic retouch",
+)
+
+st.markdown("### test image 13")
+centered_image_comparison(
+    img1="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/13.jpg",
+    img2="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/13_after.jpg",
+    label1="input",
+    label2="magic retouch",
+)
+
+st.markdown("### test image 14")
+centered_image_comparison(
+    img1="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/14.jpg",
+    img2="https://automated-photo-retouching-test.s3.ap-southeast-2.amazonaws.com/client/14_after.jpg",
+    label1="input",
+    label2="magic retouch",
+)
